deb5c35d6ed37fb6df661bfa7afd2b86fd113f9a
xiaoma-max <xiaoma_max@hotmail.com>
2015-07-27 14:58:21 +1000

    Log in page init

--- SwiftAnimation/ViewController.swift
+++ SwiftAnimation/ViewController.swift
@@ -8,20 +8,74 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// A delay function
+func delay(#seconds: Double, completion:()->()) {
+  let popTime = dispatch_time(DISPATCH_TIME_NOW, Int64( Double(NSEC_PER_SEC) * seconds ))
+  
+  dispatch_after(popTime, dispatch_get_main_queue()) {
+    completion()
+  }
+}
 
+class ViewController: UIViewController {
+  
+  // MARK: IB outlets
+  @IBOutlet weak var loginButton: UIButton!
+  @IBOutlet weak var heading: UILabel!
+  @IBOutlet weak var username: UITextField!
+  @IBOutlet weak var password: UITextField!
+  @IBOutlet weak var cloud1: UIImageView!
+  @IBOutlet weak var cloud2: UIImageView!
+  @IBOutlet weak var cloud3: UIImageView!
+  @IBOutlet weak var cloud4: UIImageView!
+  
+  // MARK: further UI
+  let spinner = UIActivityIndicatorView(activityIndicatorStyle: .WhiteLarge)
+  let status = UIImageView(image: UIImage(named: "banner"))
+  let label = UILabel()
+  let messages = ["Connecting ...", "Authorizing ...", "Sending credentials ...", "Failed"]
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    //dsadsad
-    //dsadsa
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    //set up the UI
+    loginButton.layer.cornerRadius = 8.0
+    loginButton.layer.masksToBounds = true
+    
+    spinner.frame = CGRect(x: -20.0, y: 6.0, width: 20.0, height: 20.0)
+    spinner.startAnimating()
+    spinner.alpha = 0.0
+    loginButton.addSubview(spinner)
+    
+    status.hidden = true
+    status.center = loginButton.center
+    view.addSubview(status)
+    
+    label.frame = CGRect(x: 0.0, y: 0.0, width: status.frame.size.width, height: status.frame.size.height)
+    label.font = UIFont(name: "HelveticaNeue", size: 18.0)
+    label.textColor = UIColor(red: 0.89, green: 0.38, blue: 0.0, alpha: 1.0)
+    label.textAlignment = .Center
+    status.addSubview(label)
+    
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
+  override func viewWillAppear(animated: Bool) {
+    super.viewWillAppear(animated)
+    
+  }
+  
+  override func viewDidAppear(animated: Bool) {
+    super.viewDidAppear(animated)
+    
+  }
+  
+  // MARK: further methods
+  @IBAction func login(sender: AnyObject) {
+  }
+  
 }
 
